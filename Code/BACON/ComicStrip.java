6117c6794af9e74d264acffba132f96d2f89bf52
stark3 <stark3@4cbb5636-1679-11df-a534-0d358c1f5519>
2010-04-02 15:32:19 +0000

    Adding the BACON directory.  Fleshed out methods, added Javadoc comments, and modified the GPL license header in ComicStrip.java

---
+++ Code/BACON/ComicStrip.java
@@ -0,0 +1,85 @@
+/**
+ * BACON (sdd.bacon@gmail.com)
+ *
+ * ComicStrip - This class provides an abstraction for a displayable Comic
+ * Strip Image.
+ *
+ * Copyright (c) 2010
+ * @author David Pizzuto, Seamus Reynolds, Matt Schoen, Michael Stark
+ * All Rights Reserved
+ *
+ * http://code.google.com/p/bacon/
+ *
+ * @version 0.1, 04/02/10
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+package BACON;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class ComicStrip {
+	private Image comicStripImage;		// The actual Image of the comic strip.
+	
+	/**
+	 * Sets the comicStripImage object to null.
+	 */
+	public ComicStrip() {
+		comicStripImage = null;
+	}
+	
+	/**
+	 * Loads the image specified by fileName into comicStripImage.
+	 *
+	 * @param fileName the full path to an image file.
+	 */
+	public ComicStrip(String fileName) {
+		comicStripImage = null;
+		try {
+			comicStripImage = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+			// Here, we should handle the exception - maybe display an error
+			// message like "Could not load file fileName".
+		}
+	}
+	
+	/**
+	 * Loads the image specified by fileName into comicStripImage.
+	 *
+	 * @param fileName the full path to an image file.
+	 */
+	public void loadImage(String fileName) {
+		comicStripImage = null;
+		try {
+			comicStripImage = ImageIO.read(new File(fileName));
+		} catch (IOException e) {
+			// Here, we should handle the exception - maybe display an error
+			// message like "Could not load file fileName".
+		}
+	}
+	
+	/**
+	 * Returns the comic strip image as an Image object.
+	 *
+	 * @return 	the Comic Strip Image.
+	 * @see 	Image
+	 */
+	public Image getComicStripImage() {
+		return comicStripImage;
+	}
+}
