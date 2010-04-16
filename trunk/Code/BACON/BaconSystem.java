179fca228f3800b9c02173af5d9c14d4ea94c09e
pizzutosan <pizzutosan@4cbb5636-1679-11df-a534-0d358c1f5519>
2010-04-16 15:46:07 +0000

    Initial testing framework for ComicDatabase.

--- trunk/Code/BACON/BaconSystem.java
+++ trunk/Code/BACON/BaconSystem.java
@@ -2,7 +2,7 @@
  * BACON (sdd.bacon@gmail.com)
  *
  * BaconSystem - The backend structure of the BACON Project.
- * 
+ *
  * Copyright (c) 2010
  * @author David Pizzuto, Seamus Reynolds, Matt Schoen, Michael Stark
  * All Rights Reserved
@@ -24,7 +24,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 package BACON;
 
 import java.io.IOException;
@@ -41,10 +41,10 @@ import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLEditorKit;
 
 public class BaconSystem {
-    
+
     /**
      * Reads a webpage and returns a map of all the HTML elements.
-     * 
+     *
      * @param  url  the URL of the webpage to read
      * @return      a HashMap of HTML tag types to lists of their use
      */
@@ -52,55 +52,54 @@ public class BaconSystem {
         TagMapper tm = TagMapper.parse(url);
         return tm != null ? tm.getTagMap() : null;
     }
-    
+
     /**
      * Reads a webpage and returns a list of all the image links found.
-     * 
+     *
      * @param  url  the URL of the webpage to read
      * @return      a HashMap of HTML tag types to lists of their use
      */
     public static ArrayList<String> getImageURLs(String url) {
         HashMap<HTML.Tag, ArrayList<AttributeSet>> tagMap = getTagMap(url);
         if(tagMap == null) return null;
-        
+
         ArrayList<AttributeSet> imgs = tagMap.get(HTML.Tag.IMG);
         if(imgs == null) return new ArrayList<String>();
-        
+
         ArrayList<String> urls = new ArrayList<String>(imgs.size());
         for(AttributeSet s: imgs) {
             urls.add(s.getAttribute(HTML.Attribute.SRC).toString());
         }
-        
+
         return urls;
     }
-    
+
     /**
      * A wrapper class around the HTMLEditorKit used to make the getParser method public.
      */
     private static class ParserGetter extends HTMLEditorKit {
         /**
-         * 
+         *
          */
         public HTMLEditorKit.Parser getParser() {
             return super.getParser();
         }
     }
-    
+
     private static class TagMapper extends HTMLEditorKit.ParserCallback {
         private HashMap<HTML.Tag, ArrayList<AttributeSet>> tagMap; //The HTML tag map being created
-        
+
          /**
-         * 
+         * This constructor is private to being called by this class's methods.
          */
         private TagMapper(HashMap<HTML.Tag, ArrayList<AttributeSet>> tagMap) {
             this.tagMap = tagMap;
         }
-        
+
          /**
-         * 
+         * @param urlPath The path to the URL to be parsed
          */
-        public TagMapper(String urlPath)
-        {
+        public TagMapper(String urlPath) {
             HTMLEditorKit.Parser parser = new ParserGetter().getParser();
             URL url;
             try {
@@ -109,7 +108,7 @@ public class BaconSystem {
                 System.err.println("URL Exception Thrown: " + e);
                 return;
             }
-            
+
             try {
                 InputStreamReader r = new InputStreamReader(url.openStream(), "ISO-8859-1");
                 tagMap = new HashMap<HTML.Tag, ArrayList<AttributeSet>>();
@@ -118,9 +117,9 @@ public class BaconSystem {
                 System.err.println("IO Exception Thrown: " + e);
             }
         }
-        
+
          /**
-         * 
+         *
          */
         private void mapTag(HTML.Tag t, AttributeSet a) {
             ArrayList<AttributeSet> list = tagMap.get(t);
@@ -130,23 +129,23 @@ public class BaconSystem {
             }
             list.add(a.copyAttributes());
         }
-        
+
          /**
-         * 
+         *
          */
         public void handleSimpleTag(HTML.Tag t, MutableAttributeSet a, int pos) {
             mapTag(t, a);
         }
-        
+
          /**
-         * 
+         * @return The map of tags
          */
         public HashMap<HTML.Tag, ArrayList<AttributeSet>> getTagMap() {
             return tagMap;
         }
-        
+
          /**
-         * 
+         *
          */
         public static TagMapper parse(String urlPath) {
             HTMLEditorKit.Parser parser = new ParserGetter().getParser();
@@ -157,7 +156,7 @@ public class BaconSystem {
                 System.err.println("URL Exception Thrown: " + e);
                 return null;
             }
-            
+
             try {
                 InputStreamReader r = new InputStreamReader(url.openStream(), "ISO-8859-1");
                 TagMapper tm = new TagMapper(new HashMap<HTML.Tag, ArrayList<AttributeSet>>());
@@ -169,16 +168,5 @@ public class BaconSystem {
             return null;
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
