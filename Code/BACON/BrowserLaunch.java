60df27ae90ebd3cc2da777407adf7a49c9ea9647
pizzutosan <pizzutosan@4cbb5636-1679-11df-a534-0d358c1f5519>
2010-04-28 01:44:32 +0000

    Made BrowserLaunch a third-party class and updated sequence diagram to reflect.

--- Code/BACON/BrowserLaunch.java
+++ Code/BACON/BrowserLaunch.java
@@ -1,13 +1,14 @@
 /**
+ * Modified from Dem Pilafian's Bare Bones Browser Launch v3.0, available in the public
+ * domain at http://www.centerkey.com/java/browser/BareBonesBrowserLaunch.java.
+ *
  * BACON (sdd.bacon@gmail.com)
  *
- * BrowserLaunch - This class displays the URL for the currently displayed webcomic
+ * BrowserLaunch - Launches the user's preferred browser.
  *
- * Copyright (c) 2010
+ * Modifications copyright (c) 2010
  * @author David Pizzuto, Seamus Reynolds, Matt Schoen, Michael Stark
  * All Rights Reserved
- *
- * @version 0.1, 04/02/10
  *
  * http://code.google.com/p/bacon/
  *
@@ -25,14 +26,69 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- package BACON;
+package BACON;
 
- import java.awt.Desktop;
- import java.io.IOException;
- import java.net.URI;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import java.util.Arrays;
 
- public class BrowserLaunch {
-     public static void loadBrowser(String path) throws IOException {
-        Desktop.getDesktop().browse(URI.create(path));
-     }
- }
+public class BrowserLaunch {
+
+    private static final String[] browsers = { "google-chrome", "firefox", "opera",
+        "konqueror", "epiphany", "seamonkey", "galeon", "kazehakase", "mozilla" };
+    private static final String errMsg = "Error attempting to launch web browser";
+
+    /**
+     * Opens a URL in the user's preferred browser if possible.
+     * @param url The URL to be opened
+     * @throws IOException If the browser has some sort of error. Other errors will be met with a JOptionPane.
+     */
+    public static void openUrl(String url) throws IOException {
+        try {  // attempt to use Desktop library from JDK 1.6+ (even if on 1.5)
+            Class<?> d = Class.forName("java.awt.Desktop");
+            d.getDeclaredMethod("browse", new Class[] {java.net.URI.class}).invoke(
+                d.getDeclaredMethod("getDesktop").invoke(null),
+                    new Object[] {java.net.URI.create(url)});
+            // above code mimics:
+            //   java.awt.Desktop.getDesktop().browse(java.net.URI.create(url));
+            // through the use of reflection
+        } catch (ClassNotFoundException e) {  // library not available or failed
+            launchPreferredBrowser(url);
+        } catch (Exception e) {
+            SwingInput.displayErrorMessage(e.getMessage());
+        }
+    }
+
+    /**
+     * Hunts down and launches the user's preferred browser by combing through the OS.
+     * @param url The URL to be opened.
+     */
+    private static void launchPreferredBrowser(String url) {
+        String osName = System.getProperty("os.name");
+        try {
+            if (osName.startsWith("Mac OS")) {
+            Class.forName("com.apple.eio.FileManager").getDeclaredMethod(
+                "openURL", new Class[] {String.class}).invoke(null,
+                new Object[] {url});
+            } else if (osName.startsWith("Windows")) {
+               Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else { // assume Unix or Linux
+                boolean found = false;
+                for (String browser : browsers) {
+                    if (!found) {
+                        found = Runtime.getRuntime().exec(
+                            new String[] {"which", browser}).waitFor() == 0;
+                        if (found) {
+                            Runtime.getRuntime().exec(new String[] {browser, url});
+                        }
+                    }
+                }
+                if (!found) {
+                    throw new Exception(Arrays.toString(browsers));
+                }
+            }
+        } catch (Exception e) {
+            SwingInput.displayErrorMessage(e.getMessage());
+        }
+    }
+}
